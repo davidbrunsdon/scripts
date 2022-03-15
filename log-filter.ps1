741ed18c9e3c7880b0f1edb7f202cb7ebcaec01a
davidbrunsdon <91513423+davidbrunsdon@users.noreply.github.com>
2022-03-15 14:18:42 -0700

    Update log-filter.ps1

--- log-filter.ps1
+++ log-filter.ps1
@@ -1,7 +1,8 @@
 #This script recursively searches logs folders for a keyword.
 #It's configured to find occurances of missing RC-Archive data.
+#Created by david@darkivy.io
 
-
+#User Variables
 $LogLocation = 'C:\ProgramData\Reliable Controls\RC-Archive\logs'
 $UserPath = "$($env:UserProfile)\Desktop\filter_output.txt"
 $String = @(
@@ -10,8 +11,8 @@ $String = @(
     'Number of routable Addresses: 0'
 )
 
+#Search LogLocation for list of strings and print to file on Desktop
 Set-Location -Path $LogLocation
-
 foreach ( $item in $String ){
     Write-Output $item | Out-file $UserPath -Append
     Get-ChildItem -Recurse | Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays(-7)} | Select-String $item -List | Select Path | Out-File $UserPath -Append
